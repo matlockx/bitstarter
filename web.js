87bb89b69e658ffb381d309fb65264f24c06dad6
martin <m.joehren@gmail.com>
2013-07-05 18:45:37 +0000

    added read from file

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
-
+var fs = require('fs');
+var infile = "index.html";
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  fs.readFile(infile, function(err, data) {
+     response.send(data);
+  }
+  );
+
 });
 
 var port = process.env.PORT || 5000;
